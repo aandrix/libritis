a1e7efd6bfa3c67906db6ac7f5c7df9ef1aa7288
aandrix <andrea.giordano.s@itiscuneo.eu>
2024-05-27 18:37:36 +0200

    oigbnsdk.jnwerok

---
+++ programmaritiro.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+	<?php
+		session_start();
+		session_unset();
+		session_destroy();
+	?>
+  	<title>login libritis</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Prenota il ritiro</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				
+		      	<form action="./php/logincheck.php" method="POST"class="signin-form">
+		      		<div class="form-group">
+		      			<input type="date" class="form-control" placeholder="Username" name="user" id="user" required>
+		      		</div>
+	            
+	            <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary submit px-3">prenota</button>
+	            </div>
+	            <div class="form-group d-md-flex">
+	            	
+	            </div>
+	          </form>
+	       
+	        
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
